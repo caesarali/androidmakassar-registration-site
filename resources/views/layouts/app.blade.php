0e6c4eb195bf6e5c29947a4de9d3f1d7348afd11
Caesar Ali L <caesaralilamondo@gmail.com>
2019-09-05 10:16:28 +0800

    Fix http content.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -48,7 +48,7 @@
             <nav class="navbar navbar-expand-md navbar-light bg-white @guest shadow-sm @endguest">
                 <div class="container">
                     <a class="navbar-brand py-3" href="{{ url('/') }}">
-                        <img src="http://androidmakassar.com/wp-content/uploads/2019/08/anro-6.png" alt="Android Makassar">
+                        <img src="https://androidmakassar.com/wp-content/uploads/2019/08/anro-6.png" alt="Android Makassar">
                     </a>
                     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                         <span class="navbar-toggler-icon"></span>
--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -2,7 +2,7 @@
     <td class="header">
         <a href="{{ $url }}">
             {{-- {{ $slot }} --}}
-            <img src="http://androidmakassar.com/wp-content/uploads/2019/08/anro-6.png" alt="Android Makassar">
+            <img src="https://androidmakassar.com/wp-content/uploads/2019/08/anro-6.png" alt="Android Makassar">
         </a>
     </td>
 </tr>
