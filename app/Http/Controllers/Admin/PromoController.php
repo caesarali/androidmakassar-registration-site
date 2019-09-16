d2e4ca3d744b4b460c4cac114d6e206de459b0b0
Caesar Ali L <caesaralilamondo@gmail.com>
2019-09-16 15:40:08 +0800

    .

--- app/Http/Controllers/Admin/PromoController.php
+++ app/Http/Controllers/Admin/PromoController.php
@@ -23,12 +23,41 @@ class PromoController extends Controller
 
     public function store(Request $request)
     {
+        $request->validate([
+            'code' => 'required|string|unique:promo',
+            'name' => 'nullable|string',
+            'description' => 'nullable|string',
+            'event_id' => 'required|integer',
+            'discount' => 'required|integer',
+            'from_date' => 'required|date',
+            'to_date' => 'required|date'
+        ]);
 
+        $promo = Promo::create($request->all());
+
+        return redirect()->back()->withSuccess('Promo has been created.');
     }
 
     public function edit(Promo $promo)
     {
         $events = Event::all();
         return view('pages.admin.promo.edit', compact('promo', 'events'));
+    }
+
+    public function update(Request $request, Promo $promo)
+    {
+        $request->validate([
+            'code' => 'required|string|unique:promo,code,' . $promo->id,
+            'name' => 'nullable|string',
+            'description' => 'nullable|string',
+            'event_id' => 'required|integer',
+            'discount' => 'required|integer',
+            'from_date' => 'required|date',
+            'to_date' => 'required|date'
+        ]);
+
+        $promo->update($request->all());
+
+        return redirect()->back()->withSuccess('Saved.');
     }
 }
